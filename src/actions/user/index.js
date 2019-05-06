611f98819b66dd236f52d17871fe070b6d366140
Juan Carlos Farah <farah.juancarlos@gmail.com>
2019-05-06 08:20:01 +0200

    fix: do not make geolocation call if offline
    closes #69

--- src/actions/user/index.js
+++ src/actions/user/index.js
@@ -7,27 +7,30 @@ import {
 } from '../../config/messages';
 
 const getGeolocation = async () => async dispatch => {
-  try {
-    const geolocation = await getCurrentPosition();
-    const payload = {
-      coords: {
-        accuracy: geolocation.coords.accuracy,
-        altitude: geolocation.coords.altitude,
-        altitudeAccuracy: geolocation.coords.altitudeAccuracy,
-        heading: geolocation.coords.heading,
-        latitude: geolocation.coords.latitude,
-        longitude: geolocation.coords.longitude,
-        speed: geolocation.coords.speed,
-      },
-      timestamp: geolocation.timestamp,
-    };
-    dispatch({
-      type: GET_GEOLOCATION_SUCCEEDED,
-      payload,
-    });
-  } catch (e) {
-    console.error(e);
-    toastr.error(ERROR_MESSAGE_HEADER, ERROR_GETTING_GEOLOCATION);
+  // only fetch location if online
+  if (window.navigator.onLine) {
+    try {
+      const geolocation = await getCurrentPosition();
+      const payload = {
+        coords: {
+          accuracy: geolocation.coords.accuracy,
+          altitude: geolocation.coords.altitude,
+          altitudeAccuracy: geolocation.coords.altitudeAccuracy,
+          heading: geolocation.coords.heading,
+          latitude: geolocation.coords.latitude,
+          longitude: geolocation.coords.longitude,
+          speed: geolocation.coords.speed,
+        },
+        timestamp: geolocation.timestamp,
+      };
+      dispatch({
+        type: GET_GEOLOCATION_SUCCEEDED,
+        payload,
+      });
+    } catch (e) {
+      console.error(e);
+      toastr.error(ERROR_MESSAGE_HEADER, ERROR_GETTING_GEOLOCATION);
+    }
   }
 };
 
