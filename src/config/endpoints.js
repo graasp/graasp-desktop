2859b14f671eb93f0ab5033e44239b60975a94ba
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2019-06-14 16:33:28 +0200

    fix: update call for offline download
    closes #93

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,6 +1,6 @@
-const { REACT_APP_GRAASP_HOST } = process.env;
+const { REACT_APP_GRAASP_API_HOST, REACT_APP_GRAASP_HOST } = process.env;
 
 export const generateGetSpaceEndpoint = id =>
-  `${REACT_APP_GRAASP_HOST}/spaces/${id}/download-offline`;
+  `${REACT_APP_GRAASP_API_HOST}/spaces/${id}/offline-structure`;
 
 export const GET_SPACES_NEARBY_ENDPOINT = `${REACT_APP_GRAASP_HOST}/spaces/nearby`;
