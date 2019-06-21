d18f4e3362a08afb209ef0951c6af8f971da7971
Filipe Boechat <filipeboechatsampaio@hotmail.com>
2019-06-21 10:55:38 -0300

    refactor: factor out getSpace listener
    Adding getSpace to listeners/index.js

--- public/app/listeners/index.js
+++ public/app/listeners/index.js
@@ -1,7 +1,9 @@
 const loadSpace = require('./loadSpace');
 const saveSpace = require('./saveSpace');
+const getSpace = require('./getSpace');
 
 module.exports = {
   loadSpace,
   saveSpace,
+  getSpace,
 };
