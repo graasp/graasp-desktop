020d3f52ddac4027fcbfc1e85fdff445e581cdd1
Filipe Boechat <47340300+fboechats@users.noreply.github.com>
2019-08-05 13:32:47 -0300

    Update showLoadSpace.js

--- public/app/listeners/showLoadSpace.js
+++ public/app/listeners/showLoadSpace.js
@@ -2,10 +2,10 @@
 const { dialog } = require('electron');
 const { RESPOND_LOAD_SPACE_PROMPT_CHANNEL } = require('../config/channels');
 
-const showLoadSpace = mainWindow => (event, options) => {
+const showLoadSpacePrompt = mainWindow => (event, options) => {
   dialog.showOpenDialog(null, options, filePaths => {
     mainWindow.webContents.send(RESPOND_LOAD_SPACE_PROMPT_CHANNEL, filePaths);
   });
 };
 
-module.exports = showLoadSpace;
+module.exports = showLoadSpacePrompt;
