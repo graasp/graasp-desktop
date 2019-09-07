7391bf5812719873bae48e9df59cd13e1478baa7
Juan Carlos Farah <farah.juancarlos@gmail.com>
2019-09-07 19:11:05 +0200

    fix: correctly import google analytics id
    closes #188

--- scripts/setup.js
+++ scripts/setup.js
@@ -4,11 +4,16 @@ const path = require('path');
 const DEFAULT_PATH = './public/';
 const NAME = 'env.json';
 
-const { SENTRY_DSN = '', GOOGLE_API_KEY = '' } = process.env;
+const {
+  SENTRY_DSN = '',
+  GOOGLE_API_KEY = '',
+  GOOGLE_ANALYTICS_ID = '',
+} = process.env;
 
 const env = JSON.stringify({
   SENTRY_DSN,
   GOOGLE_API_KEY,
+  GOOGLE_ANALYTICS_ID,
 });
 
 fs.writeFileSync(path.join(DEFAULT_PATH, NAME), env, { encoding: 'utf8' });
