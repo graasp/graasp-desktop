189cb7dcf6f3ce932534a77fff08ce486bc868e7
jakob-sikken <j.sikken@utwente.nl>
2020-02-07 14:49:57 +0100

    feat: update mapping.js to include twente apps
    Added the mappings for the Twente online to offline apps.

--- public/app/config/mapping.js
+++ public/app/config/mapping.js
@@ -1,1 +1,23 @@
-module.exports = {};
+module.exports = {
+  'https://utwente.graasp.eu/preproduction/calculator/build/calculator.html':
+    'https://utwente.graasp.eu/preproduction/calculator/build/calculator.one.html',
+  'https://utwente.graasp.eu/preproduction/hypothesis/build/hypothesis.html':
+    'https://utwente.graasp.eu/preproduction/hypothesis/build/hypothesis.one.html',
+  'https://utwente.graasp.eu/preproduction/observationTool/build/observationTool.html':
+    'https://utwente.graasp.eu/preproduction/observationTool/build/observationTool.one.html',
+  'https://utwente.graasp.eu/preproduction/refquest/build/quizTool.html':
+    'https://utwente.graasp.eu/preproduction/refquest/build/quizTool.one.html',
+  'https://utwente.graasp.eu/preproduction/tableTool/build/tableTool.html':
+    'https://utwente.graasp.eu/preproduction/tableTool/build/tableTool.one.html',
+
+  'https://utwente.graasp.eu/production/calculator/build/calculator.html':
+    'https://utwente.graasp.eu/production/calculator/build/calculator.one.html',
+  'https://utwente.graasp.eu/production/hypothesis/build/hypothesis.html':
+    'https://utwente.graasp.eu/production/hypothesis/build/hypothesis.one.html',
+  'https://utwente.graasp.eu/production/observationTool/build/observationTool.html':
+    'https://utwente.graasp.eu/production/observationTool/build/observationTool.one.html',
+  'https://utwente.graasp.eu/production/refquest/build/quizTool.html':
+    'https://utwente.graasp.eu/production/refquest/build/quizTool.one.html',
+  'https://utwente.graasp.eu/production/tableTool/build/tableTool.html':
+    'https://utwente.graasp.eu/production/tableTool/build/tableTool.one.html',
+};
