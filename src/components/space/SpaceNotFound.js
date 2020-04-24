9990fbaeb77248ba55e5d6f30e1f2e1bc0f366ea
pyphilia <pyphilia@gmail.com>
2020-04-24 10:43:06 +0200

    refactor: refactor SpaceNotFound

--- src/components/space/SpaceNotFound.js
+++ src/components/space/SpaceNotFound.js
@@ -1,142 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import { withTranslation } from 'react-i18next';
 import Button from '@material-ui/core//Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import classNames from 'classnames';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { withStyles } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-import MainMenu from '../common/MainMenu';
 import Styles from '../../Styles';
 import { HOME_PATH, VISIT_PATH } from '../../config/paths';
+import Main from '../common/Main';
 
-class SpaceNotFound extends Component {
-  state = {
-    open: false,
-  };
-
-  static propTypes = {
-    t: PropTypes.func.isRequired,
-    classes: PropTypes.shape({
-      root: PropTypes.string.isRequired,
-      appBar: PropTypes.string.isRequired,
-      appBarShift: PropTypes.string.isRequired,
-      menuButton: PropTypes.string.isRequired,
-      hide: PropTypes.string.isRequired,
-      drawer: PropTypes.string.isRequired,
-      drawerPaper: PropTypes.string.isRequired,
-      drawerHeader: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      contentShift: PropTypes.string.isRequired,
-      button: PropTypes.string.isRequired,
-    }).isRequired,
-    theme: PropTypes.shape({
-      direction: PropTypes.string.isRequired,
-    }).isRequired,
-    history: PropTypes.shape({
-      length: PropTypes.number.isRequired,
-      replace: PropTypes.func.isRequired,
-    }).isRequired,
-  };
-
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { open } = this.state;
-    const {
-      history: { replace },
-      classes,
-      theme,
-      t,
-    } = this.props;
-    return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
+const SpaceNotFound = ({ history: { replace }, classes, t }) => {
+  return (
+    <Main fullScreen>
+      <div>
+        <Typography
+          align="center"
+          variant="h5"
+          color="inherit"
+          style={{ margin: '2rem' }}
         >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
+          {t('Space Not Found')}
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={() => replace(HOME_PATH)}
         >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={this.handleDrawerClose}>
-              {theme.direction === 'ltr' ? (
-                <ChevronLeftIcon />
-              ) : (
-                <ChevronRightIcon />
-              )}
-            </IconButton>
-          </div>
-          <Divider />
-          <MainMenu />
-        </Drawer>
-        <main
-          className={classNames(classes.content, {
-            [classes.contentShift]: open,
-          })}
+          {t('Home')}
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={() => replace(VISIT_PATH)}
         >
-          <div className={classes.drawerHeader} />
-          <div>
-            <Typography variant="h5" color="inherit" style={{ margin: '2rem' }}>
-              {t('Space Not Found')}
-            </Typography>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              onClick={() => replace(HOME_PATH)}
-            >
-              {t('Home')}
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              onClick={() => replace(VISIT_PATH)}
-            >
-              {t('Visit Another Space')}
-            </Button>
-          </div>
-        </main>
+          {t('Visit Another Space')}
+        </Button>
       </div>
-    );
-  }
-}
+    </Main>
+  );
+};
+
+SpaceNotFound.propTypes = {
+  t: PropTypes.func.isRequired,
+  classes: PropTypes.shape({
+    root: PropTypes.string.isRequired,
+    appBar: PropTypes.string.isRequired,
+    appBarShift: PropTypes.string.isRequired,
+    menuButton: PropTypes.string.isRequired,
+    hide: PropTypes.string.isRequired,
+    drawer: PropTypes.string.isRequired,
+    drawerPaper: PropTypes.string.isRequired,
+    drawerHeader: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    contentShift: PropTypes.string.isRequired,
+    button: PropTypes.string.isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    length: PropTypes.number.isRequired,
+    replace: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 const StyledComponent = withStyles(Styles, { withTheme: true })(SpaceNotFound);
 
