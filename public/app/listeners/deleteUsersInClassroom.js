78e02847613e0d8c735df09f83bac949a0d3ca4f
pyphilia <pyphilia@gmail.com>
2020-12-16 16:33:04 +0100

    fix: delete related resources when deleting user in classroom

--- public/app/listeners/deleteUsersInClassroom.js
+++ public/app/listeners/deleteUsersInClassroom.js
@@ -1,4 +1,9 @@
-const { USERS_COLLECTION, CLASSROOMS_COLLECTION } = require('../db');
+const {
+  USERS_COLLECTION,
+  CLASSROOMS_COLLECTION,
+  ACTIONS_COLLECTION,
+  APP_INSTANCE_RESOURCES_COLLECTION,
+} = require('../db');
 const { DELETE_USERS_IN_CLASSROOM_CHANNEL } = require('../config/channels');
 const { ERROR_GENERAL, ERROR_NO_USER_TO_DELETE } = require('../config/errors');
 
@@ -19,13 +24,20 @@ const deleteUsersInClassroom = (mainWindow, db) => async (
   }
 
   try {
-    const usersInClassroom = db
-      .get(CLASSROOMS_COLLECTION)
-      .find({ id: classroomId })
-      .get(USERS_COLLECTION);
+    const classroom = db.get(CLASSROOMS_COLLECTION).find({ id: classroomId });
+    const usersInClassroom = classroom.get(USERS_COLLECTION);
 
     users.forEach(({ id }) => {
       usersInClassroom.remove({ id }).write();
+
+      // remove related actions
+      classroom.get(ACTIONS_COLLECTION).remove({ user: id }).write();
+
+      // remove related appInstanceResources
+      classroom
+        .get(APP_INSTANCE_RESOURCES_COLLECTION)
+        .remove({ user: id })
+        .write();
     });
 
     return mainWindow.webContents.send(DELETE_USERS_IN_CLASSROOM_CHANNEL);
